1414ca9d2689a5cf4ce23ed8ac4b9d1833fc7803
JoyMarocho <joymarocho@gmail.com>
2022-06-13 01:32:36 +0300

    creating class profile

--- ratings/models.py
+++ ratings/models.py
@@ -38,3 +38,29 @@ class Rating(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=50, blank=True)
+    occupation = models.CharField(max_length=70, blank=True)
+    date_updated = models.DateField(default=date.today)
+    profile_picture = CloudinaryField('image', blank=True)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+    def save(self,*args,**kwargs):
+        super().save()
+        img = Image.open(self.profile_picture)
+        if img.height > 100 or img.width >100:
+            new_img = (100,100)
+            img.thumbnail(new_img)
+            img.save(self.profile_picture)
+
+    def get_absolute_url(self):
+        return reverse('profileupdate')
+
+    def save_profile(self):
+        self.save()
