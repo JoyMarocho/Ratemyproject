4f58e17645935328c0937394945f2eb4d95fa690
JoyMarocho <joymarocho@gmail.com>
2022-06-13 01:13:44 +0300

    creating admin panel

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project, Rating,Profile
+
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Rating)
+admin.site.register(Profile)
