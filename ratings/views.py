600d40d1b50025d14ccd3e8958c3508d2996b797
JoyMarocho <joymarocho@gmail.com>
2022-06-13 13:21:00 +0300

    creating register function

--- ratings/views.py
+++ ratings/views.py
@@ -20,3 +20,16 @@ from django.contrib.messages.views import SuccessMessageMixin
 def index(request):
     message = "Welcome to Rate-My-Project"
     return render(request,'index.html',{"message": message})
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        messages.success(request, f'Registration Successful.')
+        return redirect('homepage')
+    messages.error(request, f'Unsuccessful registration. Invalid information.')
+    form = NewUserForm()
+    return render(request, 'registration/registration_form.html', {"registration_form": form})
