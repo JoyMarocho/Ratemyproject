71adda0d15add3d4139f4d6d34c8a83a96e17fa5
JoyMarocho <joymarocho@gmail.com>
2022-06-13 13:18:40 +0300

    routuing pages

---
+++ ratings/urls.py
@@ -0,0 +1,20 @@
+from django.urls import re_path as url, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.ProjectListView.as_view(), name='homepage'),
+    url(r'register', views.register_user, name='register'),
+    url(r'login', views.login_user, name='login'),
+    url(r'logout', views.logout_user, name='logout'),
+    url(r'new/project/$', views.ProjectCreateView.as_view(), name='project_add'),
+    #url(r'^profile/(?P<user_id>\d+)', views.profile, name='profiles'),
+    url(r'profile/', views.profile, name='profileupdate'),
+    url(r'update_profile', views.update_profile, name='update_profile'),
+    url(r'^api/project/$', views.ListProjects.as_view()),
+    url(r'^api/profile/$', views.ListUserProfile.as_view()),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
