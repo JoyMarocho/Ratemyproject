8f2a9c91e9ef2cefe58971830ca6265b7ecc52d3
JoyMarocho <joymarocho@gmail.com>
2022-06-13 16:15:44 +0300

    Creating a serializer

---
+++ ratings/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Project,Profile
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Project
+        fields = ('title','image','description','link')
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profile
+        fields = ('user','bio','profile_picture')
+
+        def get_profile_picture(self,obj):
+            if obj.profile_picture:
+                return obj.profile_picture
+            return 'https://www.wallpaperflare.com/boruto-digital-wallpaper-uzumaki-boruto-jogan-one-person-real-people-wallpaper-hxetu'
