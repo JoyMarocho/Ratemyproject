f9b80a41f16b62dbfd18f56151ea518394ecc309
JoyMarocho <joymarocho@gmail.com>
2022-06-13 13:56:26 +0300

    creating login and logout function

--- ratings/views.py
+++ ratings/views.py
@@ -33,3 +33,27 @@ def register_user(request):
     messages.error(request, f'Unsuccessful registration. Invalid information.')
     form = NewUserForm()
     return render(request, 'registration/registration_form.html', {"registration_form": form})
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+            return redirect('homepage')
+        else:
+            messages.error(request, f'Invalid Username or password')
+    else:
+            messages.error(request, f'Invalid username or password')
+    form = AuthenticationForm()
+    return render(request, 'registration/login_form.html', {"login_form": form})
+
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, f'You have successfully logged out.')
+    return redirect('login')
